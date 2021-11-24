c645339f71998799eabc3e659b7c400707020e2d
yaasien-s <66425757+yaasien-s@users.noreply.github.com>
2021-11-24 12:19:24 +0200

    Add files via upload
    commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let sum ="";
+
+function addToSum(operator) {
+    sum = sum + operator;
+    console.log(sum);
+    document.getElementById('output').innerText = sum;
+}
+
+function evaluateSum() {
+    sum = eval(sum)
+    document.getElementById('output').innerText = sum;
+
+}
+
+
+function ClearSum() {
+    sum = "";
+    document.getElementById('output').innerText = sum;
+}
+
+function deleteChar() {
+    sum = sum.slice(0,-1);
+    document.getElementById('output').innerText = sum;
+}
